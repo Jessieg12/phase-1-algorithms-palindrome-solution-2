7e20644a25260945c04107f2e75941890ebe5301
Jessie Guzman <jessieguzman777@gmail.com>
2022-12-02 10:29:15 -0600

    completed

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let i = 0; i <word.length/2;i++){
+    const j = word.length -1 -i
+    const startChar = word[i]
+    const endChar = word[j]
+    if(startChar !== endChar) return false
+  }
+  return true
 }
 
 /* 
+  We want to have a start and an end point that will hold the value of a letter in our word
+  then we want to iterate through the word and divide the word into 2 pieces to determain when to stop iteration
+  we want to return false if the letters dont match
+  we want to return true if the letters in the variables match
   Add your pseudocode here
 */
 
